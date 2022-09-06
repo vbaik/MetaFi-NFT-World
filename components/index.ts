8a77cb22b05fc2f2d52ddc07ced0125dc499fb83
Vivian Baik <59579994+vbaik@users.noreply.github.com>
2022-09-06 14:46:18 -0300

    Nft item and list components created

---
+++ components/index.ts
@@ -0,0 +1,4 @@
+export { default as Navbar } from './ui/navbar';
+export { default as BaseLayout } from './ui/layout/BaseLayout';
+export { default as NftItem } from './nft/item/index';
+export { default as NftList } from './nft/list/index';
