ddefc73d18217b689a8c8989b0d27e50925fbd78
Vivian Baik <59579994+vbaik@users.noreply.github.com>
2022-09-07 21:33:48 -0300

    Profile page and link added

---
+++ pages/profile.tsx
@@ -0,0 +1,8 @@
+import { NextPage } from 'next';
+import { BaseLayout } from '../components';
+
+const Profile: NextPage = () => {
+  return <BaseLayout>Profile Page</BaseLayout>;
+};
+
+export default Profile;
