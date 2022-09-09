5489dfd618fa8d9aa7c9ecaf869c66f906c82700
Vivian Baik <59579994+vbaik@users.noreply.github.com>
2022-09-08 21:04:42 -0300

    Web3Provider Component added with a test data

---
+++ components/providers/web3/index.tsx
@@ -0,0 +1,23 @@
+import { createContext, FunctionComponent, useContext, useState } from 'react';
+
+//밑에꺼 꼭 넣어야 children에서 에러안남.
+//In the new version of React, when you pass children, you need to explicitly specify a type for it:
+interface Props {
+  children: React.ReactNode;
+}
+
+const Web3Context = createContext<any>(null);
+
+const Web3Provider: FunctionComponent<Props> = ({ children }) => {
+  const [web3Api, setWeb3Api] = useState({ test: 'Hello Provider!' });
+
+  return (
+    <Web3Context.Provider value={web3Api}>{children}</Web3Context.Provider>
+  );
+};
+
+export function useWeb3() {
+  return useContext(Web3Context);
+}
+
+export default Web3Provider;
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,10 +2,14 @@ import type { NextPage } from 'next'; //NextPage type is next.js's built-in type
 import { BaseLayout, NftList } from '../components';
 import nftData from '../content/meta.json';
 import { NftMetaData } from '@_types/nft';
+import { useWeb3 } from 'components/providers/web3';
 
 const Home: NextPage = () => {
+  const { test } = useWeb3();
+
   return (
     <BaseLayout>
+      {test}
       <div className='relative bg-gray-50 pt-16 pb-20 px-4 sm:px-6 lg:pt-24 lg:pb-28 lg:px-8'>
         <div className='absolute inset-0'>
           <div className='bg-white h-1/3 sm:h-2/3' />
