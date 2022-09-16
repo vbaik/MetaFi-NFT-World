2e4f2f3d764bb6ca92e5a1c8474799e35c81932c
Vivian Baik <59579994+vbaik@users.noreply.github.com>
2022-09-15 22:28:25 -0300

    Generic types added for CryptoHookFactory

--- components/hooks/web3/useAccount.ts
+++ components/hooks/web3/useAccount.ts
@@ -3,7 +3,7 @@ import { CryptoHookFactory } from "@_types/hooks";
 
 // deps means dependencies -> provider, ethereum, contract all from web3State
 // hookFactory is a function that returns a function. So the format is () => () => {}
-export const hookFactory: CryptoHookFactory = (deps: any) => (params: any) => {
+export const hookFactory: CryptoHookFactory<string, string>= (deps) => (params) => {
   const swrRes = useSWR("web3/useAccount", () => {
     console.log({deps});
     console.log({params});
--- types/hooks.ts
+++ types/hooks.ts
@@ -8,17 +8,26 @@ export type Web3Dependencies = {
   ethereum: MetaMaskInpageProvider; //from metamask
 };
 
-export type CryptoSWRResponse = SWRResponse; //from swr
+export type CryptoSWRResponse<D = any> = SWRResponse<D>; //from swr
 
 //funtion that returns swrResponse.
 /*밑에꺼 더 짧게 
   export type CryptoHandlerHook = (params: string) => SWRResponse;
 이렇게 써도됨.*/
-export type CryptoHandlerHook = (params: string) => CryptoSWRResponse;
+export type CryptoHandlerHook<D = any, P = any> = (
+  params: P
+) => CryptoSWRResponse<D>;
 
 //type for hookFactory
-export type CryptoHookFactory = {
+//D = data, P = parameters
+export type CryptoHookFactory<D = any, P = any> = {
   //hookFactory function: returns another function --> format은 (): ()
   //Partial써야 values in the Web3Dependencies may not be all available in the beginning하는걸 recognize 할수있음.
-  (d: Partial<Web3Dependencies>): CryptoHandlerHook;
+  (d: Partial<Web3Dependencies>): CryptoHandlerHook<D, P>;
 };
+
+/*위의 코드를 짧게 밑에처럼 쓸수도 있지만 그렇게 하면 CryptoHandlerHook 타입이 다른곳에서 따로 필요할때 못쓰니깐 위에처럼 나누는것이 좋음.
+export type CryptoHookFactory<D = any, P = any> = {
+  (d: Partial<Web3Dependencies>): (params: P) => SWRResponse<D>;
+};
+*/
