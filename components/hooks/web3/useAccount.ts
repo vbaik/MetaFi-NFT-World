9720af7f8158f136959911212442ca3025c47dd0
Vivian Baik <59579994+vbaik@users.noreply.github.com>
2022-09-17 22:29:13 -0300

    Navbar shows connect button (instead of avatar) if metamask wallet is not connected
    It also activates Metamask so the user can log in if the wallet is not connected.

--- components/hooks/web3/useAccount.ts
+++ components/hooks/web3/useAccount.ts
@@ -1,13 +1,17 @@
 import useSWR from 'swr';
 import { CryptoHookFactory } from '@_types/hooks';
 
-type AccountHookFactory = CryptoHookFactory<string>; //data는 string이고 parameter는 any니깐 표시 안써줘도 됨.
+type UseAccountResponse = {
+  connect: () => void; // function that returns nothing.
+};
+
+type AccountHookFactory = CryptoHookFactory<string, UseAccountResponse>; 
 export type UseAccountHook = ReturnType<AccountHookFactory>; //return type of AccountHookFactory type
 
 // deps means dependencies -> provider, ethereum, contract all from web3State
 // hookFactory is a function that returns a function. So the format is () => () => {}
 export const hookFactory: AccountHookFactory =
-  ({ provider }) =>
+  ({ provider, ethereum }) =>
   () => {
     const swrRes = useSWR(
       provider ? 'web3/useAccount' : null,
@@ -26,5 +30,16 @@ export const hookFactory: AccountHookFactory =
       }
     );
 
-    return swrRes;
+    const connect = async () => {
+      try {
+        ethereum?.request({ method: 'eth_requestAccounts' }); //this will open the MetaMask to connect your wallet if account is not connected.
+      } catch (e) {
+        console.error(e);
+      }
+    };
+
+    return {
+      ...swrRes,
+      connect,
+    };
   };
--- types/hooks.ts
+++ types/hooks.ts
@@ -8,22 +8,22 @@ export type Web3Dependencies = {
   ethereum: MetaMaskInpageProvider; //from metamask
 };
 
-export type CryptoSWRResponse<D = any> = SWRResponse<D>; //from swr
+export type CryptoSWRResponse<D = any, R = any> = SWRResponse<D> & R; //from swr
 
 //funtion that returns swrResponse.
 /*밑에꺼 더 짧게 
   export type CryptoHandlerHook = (params: string) => SWRResponse;
 이렇게 써도됨.*/
-export type CryptoHandlerHook<D = any, P = any> = (
+export type CryptoHandlerHook<D = any, R = any, P = any> = (
   params?: P
-) => CryptoSWRResponse<D>;
+) => CryptoSWRResponse<D, R>;
 
 //type for hookFactory
-//D = data, P = parameters
-export type CryptoHookFactory<D = any, P = any> = {
+//D = data, P = parameters, R = response
+export type CryptoHookFactory<D = any, R = any, P = any> = {
   //hookFactory function: returns another function --> format은 (): ()
   //Partial써야 values in the Web3Dependencies may not be all available in the beginning하는걸 recognize 할수있음.
-  (d: Partial<Web3Dependencies>): CryptoHandlerHook<D, P>;
+  (d: Partial<Web3Dependencies>): CryptoHandlerHook<D, R, P>;
 };
 
 /*위의 코드를 짧게 밑에처럼 쓸수도 있지만 그렇게 하면 CryptoHandlerHook 타입이 다른곳에서 따로 필요할때 못쓰니깐 위에처럼 나누는것이 좋음.
