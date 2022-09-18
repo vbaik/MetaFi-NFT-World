a4b2f097d68cfc803d8e873fcbb9f3dc542474fa
Vivian Baik <59579994+vbaik@users.noreply.github.com>
2022-09-17 21:41:57 -0300

    account data is accessed using useSWR

--- components/hooks/web3/useAccount.ts
+++ components/hooks/web3/useAccount.ts
@@ -6,13 +6,25 @@ export type UseAccountHook = ReturnType<AccountHookFactory>; //return type of Ac
 
 // deps means dependencies -> provider, ethereum, contract all from web3State
 // hookFactory is a function that returns a function. So the format is () => () => {}
-export const hookFactory: AccountHookFactory = ({provider}) => (params) => {
+export const hookFactory: AccountHookFactory =
+  ({ provider }) =>
+  () => {
     const swrRes = useSWR(
-      provider ? "web3/useAccount" : null,
-      () => {
-        return "Test User"
+      provider ? 'web3/useAccount' : null,
+      async () => {
+        const accounts = await provider!.listAccounts();
+        const account = accounts[0]; //first address from Metamask and that is the connected address.
+
+        if (!account) {
+          throw 'Cannot retreive account! Please, connect to web3 wallet.';
+        }
+
+        return account;
+      },
+      {
+        revalidateOnFocus: false,
       }
-    )
-  
+    );
+
     return swrRes;
-  }
+  };
--- pages/index.tsx
+++ pages/index.tsx
@@ -27,10 +27,10 @@ const Home: NextPage = () => {
 
   return (
     <BaseLayout>
-      {`is loading: ${isLoading}, `}
+      {/* {`is loading: ${isLoading}, `}
       {`ethereum: ${ethereum}, `}
       {`provider: ${provider}, `}
-      {`contract: ${contract}`}
+      {`contract: ${contract}`} */}
       <div className='relative bg-gray-50 pt-16 pb-20 px-4 sm:px-6 lg:pt-24 lg:pb-28 lg:px-8'>
         <div className='absolute inset-0'>
           <div className='bg-white h-1/3 sm:h-2/3' />
