93de283d180bcbf1ae868227633eafa814175f03
Vivian Baik <59579994+vbaik@users.noreply.github.com>
2022-09-25 17:47:57 -0300

    Updated NftMarket.sol to include logic to check if tokenURI exists already.

--- contracts/NftMarket.sol
+++ contracts/NftMarket.sol
@@ -10,8 +10,13 @@ contract NftMarket is ERC721URIStorage {
   Counters.Counter private _listedItems; //total number of listed NFT items in the NFTmarket.
   Counters.Counter private _tokenIds; //total number of NFT created from the smart contract.
   constructor() ERC721("CreaturesNFT", "CNFT") {} // ERC721(쓰고싶은 name of collection of NFTs, token name )
+
+  mapping(string => bool) private _usedTokenURIs; // https://whatever.com => true
+
   
   function mintToken(string memory tokenURI) public payable returns (uint) {
+    require(!tokenURIExists(tokenURI), "Token URI already exists");
+
     _tokenIds.increment();
     _listedItems.increment(); 
 
@@ -19,8 +24,13 @@ contract NftMarket is ERC721URIStorage {
 
     _safeMint(msg.sender, newTokenId);
     _setTokenURI(newTokenId, tokenURI);
+    _usedTokenURIs[tokenURI] = true;
 
     return newTokenId;
+  }
+
+   function tokenURIExists(string memory tokenURI) public view returns (bool) {
+    return _usedTokenURIs[tokenURI] == true;
   }
 }
 
