7f353cbd8a45a4f678b26165089f28878872ce39
Vivian Baik <59579994+vbaik@users.noreply.github.com>
2022-09-30 15:05:04 -0300

    UI update - disable 'List for sale' button if the active NFT is already listed.

--- pages/profile.tsx
+++ pages/profile.tsx
@@ -163,13 +163,14 @@ const Profile: NextPage = () => {
                       Download Image
                     </button>
                     <button
+                      disabled={activeNft.isListed}
                       onClick={() => {
                         nfts.listNft(activeNft.tokenId, activeNft.price); //Todo: price should be able to set by the user.
                       }}
                       type='button'
-                      className='flex-1 ml-3 bg-white py-2 px-4 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500'
+                      className='disabled:text-gray-400 disabled:cursor-not-allowed flex-1 ml-3 bg-white py-2 px-4 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500'
                     >
-                      List for Sale
+                      {activeNft.isListed ? 'Already Listed' : 'List for Sale'}
                     </button>
                   </div>
                 </div>
