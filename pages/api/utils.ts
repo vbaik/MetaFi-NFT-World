25c1dfe67c21af6f8758b5cebd15afad82ee09a1
Vivian Baik <59579994+vbaik@users.noreply.github.com>
2022-10-01 20:29:02 -0300

    retrieve smart contract on the server side using provider.

--- pages/api/utils.ts
+++ pages/api/utils.ts
@@ -2,6 +2,11 @@ import { withIronSession, Session } from 'next-iron-session';
 import contract from '../../public/contracts/NftMarket.json';
 import { NextApiRequest, NextApiResponse } from 'next';
 
+import { ethers } from 'ethers';
+import { NftMarketContract } from '@_types/nftMarketContract';
+
+const abi = contract.abi;
+
 const NETWORKS = {
   '5777': 'Ganache',
 };
@@ -28,11 +33,25 @@ export const addressCheckMiddleware = async (
   req: NextApiRequest & { session: Session },
   res: NextApiResponse
 ) => {
-  return new Promise((resolve, reject) => {
+  return new Promise(async (resolve, reject) => {
     const message = req.session.get('message-session');
-    console.log('decoded message --> ', message);
 
     if (message) {
+      const provider = new ethers.providers.JsonRpcProvider(
+        'http://127.0.0.1:7545'
+      );
+
+      //NftMarket contract
+      const contract = new ethers.Contract(
+        contractAddress,
+        abi,
+        provider
+      ) as unknown as NftMarketContract;
+
+      //to test if we can call functions from the smart contract:
+      const name = await contract.name();
+      console.log(name);
+
       resolve('Correct Address');
     } else {
       reject('Wrong Address');
