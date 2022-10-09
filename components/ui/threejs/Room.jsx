4d9f7f0ae33efbde20a48eb0eba041a16d980275
Vivian Baik <59579994+vbaik@users.noreply.github.com>
2022-10-09 00:59:52 -0300

    my-room page: added logic so that nft's x position increments

--- components/ui/threejs/Room.jsx
+++ components/ui/threejs/Room.jsx
@@ -17,30 +17,31 @@ import { Suspense } from 'react';
 import { SpawnedItemContext } from 'pages/profile';
 
 const Model = () => {
-  const gltf = useLoader(
-    GLTFLoader,
-    'https://roomdesigner.blob.core.windows.net/coinexapp/room/scene.gltf'
-  );
+  const gltf = useLoader(GLTFLoader, '/3d-models/vr_gallery_-_light_baked.glb');
   return (
     <>
-      <primitive object={gltf.scene} scale={0.3} />
+      <primitive object={gltf.scene} scale={1} position={[0, -1.3, 0]} />
     </>
   );
 };
 const url2 =
   'https://gateway.pinata.cloud/ipfs/QmWxY6RwZtrs2ErxrMG71DFLqciozgJsWSxRrxoq5Fhs3U';
 
-export const Model2 = ({ url }) => {
+export const Model2 = ({ url, position }) => {
   // const url = useContext(SpawnedItemContext);
   console.log('******', url);
   const gltf = useLoader(GLTFLoader, url);
 
   return (
     <>
-      <primitive object={gltf.scene} scale={0.3} position={[3.2, 0, 2]} />
+      <primitive object={gltf.scene} scale={0.45} position={position} />
     </>
   );
 };
+
+let x = -6;
+let y = -0.8;
+let z = 2;
 
 export default function Room() {
   const { nfts } = useOwnedNfts();
@@ -50,41 +51,26 @@ export default function Room() {
       <Suspense fallback={null}>
         <Model />
         {nfts.data.length > 0 ? (
-          nfts.data.map((nft) =>
-            nft.meta.image !== undefined ? (
-              <Model2 key={nft.meta.image} url={nft.meta.image} />
+          nfts.data.map((nft) => {
+            x += 1;
+            return nft.meta.image !== undefined ? (
+              <Model2
+                key={nft.meta.image}
+                position={[x, y, z]}
+                url={nft.meta.image}
+              />
             ) : (
               <></>
-            )
-          )
+            );
+          })
         ) : (
           <></>
         )}
-        {/* <Model2 /> */}
         <OrbitControls enableZoom={false} />
         <Environment preset='dawn' background />
       </Suspense>
     </Canvas>
   );
-
-  //********************************
-  // return (
-  //   <Canvas camera={{ position: [-10, 10, 40], fov: 50 }}>
-  //     <hemisphereLight color='white' groundColor='blue' intensity={0.75} />
-  //     <spotLight position={[50, 50, 10]} angle={0.15} penumbra={1} />
-  //     <group position={[0, -10, 0]}>
-  //       <Model position={[0, 0.25, 0]} url='3d-models/room.glb' />
-  //       <ContactShadows scale={20} blur={10} far={20} />
-  //     </group>
-  //     <OrbitControls />
-  //   </Canvas>
-  // );
-  //********************************
 }
-
-// export function Model({ url, ...props }) {
-//   const { scene } = useGLTF(url);
-//   return <primitive object={scene} {...props} />;
-// }
 
 useGLTF.preload('3d-models/room.glb');
